dc15bc7135857a2c45268bf647dd36f545f7f6a7
Manjot <physicist11102003@gmail.com>
2025-01-03 20:50:13 +0530

    made many changes

--- app/sendmail.ts
+++ app/sendmail.ts
@@ -1,36 +1,42 @@
-
-
 const nodemailer = require('nodemailer');
-const handler = async (text:string) => {
 
-
-  
+const handler = async (text: string) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', 
-    port: 587, 
+    host: 'smtp.gmail.com',
+    port: 587,
     secure: false,
     auth: {
-      user: process.env.USER, 
-      pass: process.env.PASS, 
+      user: process.env.USER,
+      pass: process.env.PASS,
     },
   });
-const to="singhmanjot9922@gmail.com";
-const subject="Regarding Portfolio query";
+
+  const to = "singhmanjot9922@gmail.com";
+  const subject = "Regarding Portfolio query";
+
   try {
-    // Send the email
-    await transporter.sendMail({
-      from: 'Your Portfolio ',
-      to, 
-      subject, 
-      text, 
+    // Wrap the sendMail function in a promise
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(
+        {
+          from: 'Your Portfolio',
+          to,
+          subject,
+          text,
+        },
+        (error: any, info: any) => {
+          if (error) {
+            return reject(error);
+          }
+          resolve(info);
+        }
+      );
     });
 
     console.log('Email sent');
-   
   } catch (error) {
     console.error(error);
-    
-}
+  }
 };
 
 export default handler;
